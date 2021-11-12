e84b59de805298315c7443955ff1d6b4cb297662
WallEren <77011722+WallEren@users.noreply.github.com>
2021-11-12 14:59:30 +0530

    Outer Diamond Pattern
    Input:
    10
    
    Output:
              *
             * *
            *   *
           *     *
          *       *
         *         *
        *           *
       *             *
      *               *
     *                 *
      *               *
       *             *
        *           *
         *         *
          *       *
           *     *
            *   *
             * *
              *

---
+++ Outer Diamond Pattern.java
@@ -0,0 +1,54 @@
+/*
+Outer Diamond Pattern
+*/
+
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) throws IOException
+	{
+	    InputStreamReader r = new InputStreamReader(System.in);
+	    BufferedReader br = new BufferedReader(r);
+	    int rowLimit = Integer.parseInt(br.readLine());
+      for(int rowIterator = 1; rowIterator <= (rowLimit*2-1); rowIterator++)  //for rows
+      {
+          if(rowIterator<=rowLimit)
+          {
+              for(int index1 = 0; index1 <= rowLimit-rowIterator; index1++)   //for spaces
+              {
+                  System.out.print(" ");
+              }
+              for(int index2 = 1; index2 <= (rowIterator*2-1); index2++)  //for pattern
+              {
+                  if(index2==1 || index2==(rowIterator*2-1))
+                  {
+                      System.out.print("*");
+                  }
+                  else
+                  {
+                      System.out.print(" ");
+                  }
+              }
+          }
+          else
+          {
+              for(int index1 = rowIterator; index1 >= rowLimit; index1--)   //for spaces
+              {
+                  System.out.print(" ");
+              }
+              for(int index2 = 1; index2 <= 2*(rowLimit-(rowIterator-rowLimit))-1; index2++)  //for pattern
+              {
+                  if(index2==1 || index2==2*(rowLimit-(rowIterator-rowLimit))-1)
+                  {
+                      System.out.print("*");
+                  }
+                  else
+                  {
+                      System.out.print(" ");
+                  }
+              }
+          }
+          System.out.println();
+      }
+	}
+}
